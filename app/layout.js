36911b77a8b510f45ce06df24e007409e7808f95
Piyush Agarwal <piyushagarwalvo@gmail.com>
2025-11-16 18:53:46 +0530

    Create landing page

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import Header from "@/components/header";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export const metadata = {
   title: "Spott",
@@ -19,25 +22,33 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
-          {/* Header */}
-          <Header />
+          <ClerkProvider
+            appearance={{
+              theme: dark,
+            }}
+          >
+            <ConvexClientProvider>
+              {/* Header */}
+              <Header />
 
-          <main className="relative min-h-screen container mx-auto pt-40 md:pt-32 ">
-            {/* glow */}
-            <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
-              <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
-              <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
-            </div>
+              <main className="relative min-h-screen container mx-auto pt-40 md:pt-32 ">
+                {/* glow */}
+                <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                  <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+                  <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
+                </div>
 
-            <div className="relative z-10 min-h-[70vh]">{children}</div>
+                <div className="relative z-10 min-h-[70vh]">{children}</div>
 
-            {/* Footer */}
-            <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto ">
-              <div className="text-sm text-gray-400">
-                Made with ❤️ by RoadsideCoder
-              </div>
-            </footer>
-          </main>
+                {/* Footer */}
+                <footer className="border-t border-gray-800/50 py-8 px-6 max-w-7xl mx-auto ">
+                  <div className="text-sm text-gray-400">
+                    Made with ❤️ by RoadsideCoder
+                  </div>
+                </footer>
+              </main>
+            </ConvexClientProvider>
+          </ClerkProvider>
         </ThemeProvider>
       </body>
     </html>
